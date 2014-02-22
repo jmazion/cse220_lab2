933cb1697615755dd0b05de17eaf0e0bb52e2a08
plan10-acme <danmartin2u@gmail.com>
2014-02-22 00:12:31 -0700

    I added a makefile.  Holton's suggestions for print.c don't seem feasible, since the only way I can see for printing an overlong line is to use a substring.  The header print function works, though.

--- Phase_2/print.c
+++ Phase_2/print.c
@@ -1,34 +1,33 @@
 #include "common.h"
 
-static void print_page_header(char source_name[], char date[]);
-
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
 {
     char save_ch;
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
+	
     
-    if (++line_count > MAX_LINES_PER_PAGE)
+    if (++line_count > MAX_LINES_PER_PAGE) // New Page
     {
-        /* Missing Code Here */
+        print_page_header(source_name_to_print, date_to_print);
+		line_count = 1;
     }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+    if (strlen(line) > MAX_PRINT_LINE_LENGTH) // If line is longer than desired
     {
-        /* Missing Code Here */
+        save_chp = (line + MAX_PRINT_LINE_LENGTH);
+		save_ch = *save_chp;
+		*save_chp = '\0';
     }
     if (save_chp)
     {
-        /* Missing Code Here */
+        *save_chp = save_ch;
     }
-    /* Missing Code Here */
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
+    puts(save_chp);
+    ++line_count; // Another line printed
 }
 static void print_page_header(char source_name[], char date[])
-{
-    static int page_number = 0;
-    
-    /* Missing Code Here */
+{    
+	static int page_number = 0; // Only initializes the first time function is called
+	printf("\nPage %d\t%s\t%s\n\n", page_number, source_name, date);
+	++page_number;
 }
