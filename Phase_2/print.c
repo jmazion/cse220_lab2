0ad2c414b6318ff3b71a93dd1162f5557c17be4a
Josh Mazion <joshmazion@gmail.com>
2014-02-22 10:25:17 -0700

    comments
    I commented on the lines you put in Daniel.
    We should try to comment ever line.
    
    But very very good job!

--- Phase_2/print.c
+++ Phase_2/print.c
@@ -7,27 +7,31 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     static int line_count = MAX_LINES_PER_PAGE;
 	
     
-    if (++line_count > MAX_LINES_PER_PAGE) // New Page
+    if (++line_count > MAX_LINES_PER_PAGE)    // New Page
     {
         print_page_header(source_name_to_print, date_to_print);
 		line_count = 1;
     }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) // If line is longer than desired
+    if (strlen(line) > MAX_PRINT_LINE_LENGTH)   // If line is longer than desired
     {
         save_chp = (line + MAX_PRINT_LINE_LENGTH);
-		save_ch = *save_chp;
-		*save_chp = '\0';
+		save_ch = *save_chp;     //adds 65 to adress pointer 
+		*save_chp = '\0';   //sets last char of a line to the null terminator
     }
-    if (save_chp)
-    {
-        *save_chp = save_ch;
+    if (save_chp)   //is only false when save_chp points to null
+    {       
+
+       /*
+       we still need a print statement here daniel ( I think)
+       */                      
+        *save_chp = save_ch;               
     }
     puts(save_chp);
-    ++line_count; // Another line printed
+    ++line_count;      // Another line printed
 }
 static void print_page_header(char source_name[], char date[])
 {    
-	static int page_number = 0; // Only initializes the first time function is called
-	printf("\nPage %d\t%s\t%s\n\n", page_number, source_name, date);
+	static int page_number = 0;         // Only initializes the first time function is called
+	printf("\nPage %d\t%s\t%s\n\n", page_number, source_name, date);        //print the page number, source name and date when and add 2 new lines below the header
 	++page_number;
 }
