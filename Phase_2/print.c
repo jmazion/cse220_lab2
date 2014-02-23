4a764ce0c01d5576afb19868047f775af4d726cb
plan10-acme <danmartin2u@gmail.com>
2014-02-23 10:49:10 -0700

    Just a tweak to the makefile.  I also added a substring method to print. Wouldn't it be better to get rid of the pointers and just have substring character arrays?

--- Phase_2/print.c
+++ Phase_2/print.c
@@ -1,10 +1,18 @@
 #include "common.h"
 
+static void print_page_header(char source_name[], char date[])
+{    
+	static int page_number = 0; // Only initializes the first time function is called
+	printf("\nPage %d\t%s\t%s\n\n", page_number, source_name, date);
+	++page_number;
+}
+
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
 {
     char save_ch;
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
+	char max_line[MAX_PRINT_LINE_LENGTH+1];
 	
     
     if (++line_count > MAX_LINES_PER_PAGE) // New Page
@@ -17,17 +25,14 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
         save_chp = (line + MAX_PRINT_LINE_LENGTH);
 		save_ch = *save_chp;
 		*save_chp = '\0';
+		strncpy(max_line, line, MAX_PRINT_LINE_LENGTH);
+		max_line[MAX_PRINT_LINE_LENGTH-1] = '\0';
     }
     if (save_chp)
     {
         *save_chp = save_ch;
     }
-    puts(save_chp);
+    printf("\n%s",max_line);
     ++line_count; // Another line printed
 }
-static void print_page_header(char source_name[], char date[])
-{    
-	static int page_number = 0; // Only initializes the first time function is called
-	printf("\nPage %d\t%s\t%s\n\n", page_number, source_name, date);
-	++page_number;
-}
+
